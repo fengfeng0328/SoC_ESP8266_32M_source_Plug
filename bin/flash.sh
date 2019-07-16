15cf0e7cba4b607fa0b1094b6e2d02281130800f
zifeng-chen <m18819838082@163.com>
2019-07-16 16:03:06 +0800

    添加烧录脚本用于烧录

---
+++ bin/flash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+esptool.py --chip esp8266 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 4MB-c1 \
+0x3fc000 esp_init_data_default.bin \
+0x3fe000 blank.bin                 \
+0x00000  boot_v1.6.bin             \
+0x01000  upgrade/user1.4096.new.6.bin       
